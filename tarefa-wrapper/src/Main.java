e022d9a3bb36f1a9afcd70f6f91c4182ad6272d4
Rafa Guerlak <guerlak@hotmail.com>
2023-10-21 01:22:13 -0300

    finished

--- tarefa-wrapper/src/Main.java
+++ tarefa-wrapper/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int age = 18;
+        System.out.println(age);
+        Integer age2 = age;
+        System.out.println(age2);
+
     }
 }
