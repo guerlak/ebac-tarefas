91206365226ae74f009e4d7612dfc412c7686a11
Rafa Guerlak <guerlak@hotmail.com>
2023-11-03 01:37:00 -0300

    delete wrapper exercise

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+
         PessoaFisica pessoaFisica = new PessoaFisica("Rafael Leite", 40, "1238718971", "12541254167" );
         PessoaJuridica pessoaJuridica = new PessoaJuridica("R-LAK", "RLAK Comercio Eletronico", "53444777000149");
 
